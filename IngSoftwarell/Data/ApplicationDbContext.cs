3d91cf0f104740c71d1542db815416aef0b72274
HAROLD1997 <36571621+HAROLD1997@users.noreply.github.com>
2022-10-01 15:00:27 -0500

    0.1

---
+++ IngSoftwarell/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using IngSoftwarell.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IngSoftwarell.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+    
+}
---
+++ IngSoftwarell/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IngSoftwarell.Models
+{
+    public class Cliente
+    {
+        
+        public string apellido { get; set; }
+        public string nombre { get; set; }
+        [Key]
+        public string usuario { get; set; }
+        public string clave { get; set; }
+        public bool estado { get; set; }
+    }
+}
